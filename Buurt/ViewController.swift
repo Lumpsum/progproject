7b1c7bc518f986182da702f4c2c5645bfeacd5f2
Martijn66 <martijn.dejong@mail.com>
2017-01-09 21:16:11 +0100

    Initial Commit

---
+++ Buurt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buurt
+//
+//  Created by Martijn de Jong on 09-01-17.
+//  Copyright © 2017 Martijn de Jong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
