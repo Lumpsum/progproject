32b81719360dd9df070ba1b7d84684c4b2e7b23e
Martijn66 <martijn.dejong@mail.com>
2017-01-17 19:01:59 +0100

    Pointers op kaart in de Explore tab

--- Buurt/ExploreViewController.swift
+++ Buurt/ExploreViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
 class ExploreViewController: UIViewController {
     
     @IBOutlet var menuButton: UIBarButtonItem!
+    @IBOutlet var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +23,22 @@ class ExploreViewController: UIViewController {
             menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        for item in currentInfo.mentions {
+            let dbLocation = item.location as Dictionary<String, String>
+            let mapPointer = MapPointer(title: item.titel, locationName: getLocation(longitude: Double(dbLocation["longitude"]!)!, latitude: Double(dbLocation["latitude"]!)!), coordinate: CLLocationCoordinate2D(latitude: Double(dbLocation["latitude"]!)!, longitude: Double(dbLocation["longitude"]!)!))
+            
+            mapView.addAnnotation(mapPointer)
+        }
+        
+        
+        
     }
-
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
