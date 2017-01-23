81c47d9cb8318967d28bcf5a27f63845839b767e
Martijn66 <martijn.dejong@mail.com>
2017-01-23 18:34:33 +0100

    View moves up when keyboard appears

--- Buurt/SingleMentionViewController.swift
+++ Buurt/SingleMentionViewController.swift
@@ -57,6 +57,9 @@ class SingleMentionViewController: UIViewController, UITableViewDelegate, UITabl
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // KEYBOARD NOTIFICATIONS
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
         
         // SHOW INFORMATION
         self.title = currentInfo.selectedMention["titel"] as! String?
@@ -78,9 +81,31 @@ class SingleMentionViewController: UIViewController, UITableViewDelegate, UITabl
             followIcon.tintColor = UIColor.green
         }
         
+    }
+    
+    
+    
+    func keyboardWillShow(notification: NSNotification) {
         
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0{
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
         
     }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0{
+                self.view.frame.origin.y += keyboardSize.height
+            }
+        }
+    }
+    
+    
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
